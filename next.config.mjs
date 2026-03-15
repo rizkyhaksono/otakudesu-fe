1005837c723d8df353eba01fdda2b46b5ad85b7a
rizkyhaksono <mrizkyhaksono@gmail.com>
2026-03-15 12:53:45 +0700

    refactor: code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "otakudesu.best",
+        hostname: "otakudesu.blog",
       },
       {
         protocol: "https",
